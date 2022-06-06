793257dd7f491aaea0a4724513227c05b824cad7
Hieu <hieuphtse151459@fpt.edu.vn>
2022-06-06 19:21:03 +0700

    update profile

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var success = "Update Successfully!"
+function notiSuccess() {
+    document.getElementById("success").innerHTML = "";
+    document.getElementById("success").innerHTML = success;
+}
+
