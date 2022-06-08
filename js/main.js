50953560e8428bca19d7c45d4858443b246a48a7
Hieu <hieuphtse151459@fpt.edu.vn>
2022-06-08 23:59:07 +0700

    update new profile

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ $(function() {
     rome(input, { time: false });
   
   });
+
+
+  const setActive = (el, active) => {
+    const formField = el.parentNode.parentNode;
+    if (active) {
+      formField.classList.add('form-field--is-active');
+    } else {
+      formField.classList.remove('form-field--is-active');
+      el.value === '' ?
+      formField.classList.remove('form-field--is-filled') :
+      formField.classList.add('form-field--is-filled');
+    }
+  };
+  
+  [].forEach.call(
+  document.querySelectorAll('.form-field__input, .form-field__textarea'),
+  el => {
+    el.onblur = () => {
+      setActive(el, false);
+    };
+    el.onfocus = () => {
+      setActive(el, true);
+    };
+  });
